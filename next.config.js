bb1709105c77452a1460c585eb9bb50295e86796
ch1nru5t <chinrust@gmail.com>
2024-10-19 17:36:40 +0530

    sourcemap

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+const IterateWebpackSourcemapUploadPlugin = require("@iterate-ai/iterate-webpack-plugin");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new IterateWebpackSourcemapUploadPlugin({
+          apiKey:
+            "340cf28bcd24d269d1555b3cf37cc5318946034cd1dcf9330786e0c299ad3087",
+          owner: "iterate-test1",
+          project: "nextui-dashboard",
+          url: "https://dev-api.iterate-ai.com/api/v0/source-maps",
+          silent: false,
+        })
+      );
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
